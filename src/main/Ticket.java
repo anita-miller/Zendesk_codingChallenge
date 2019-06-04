d51a29bb4b460c02c9bf8292c0546c553c82d52a
anita naseri <anita.naseri96@gmail.com>
2019-06-04 14:47:47 +1000

    create Ticket class to help with separation of concerns

---
+++ src/main/Ticket.java
@@ -0,0 +1,80 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Ticket {
+
+	private long requester_id;
+	private long assignee_id;
+	private ArrayList<String> tags;
+	private String subject;
+	private String description;
+	
+	/**
+	 * @return the requester_id
+	 */
+	public long getRequester_id() {
+		return requester_id;
+	}
+	/**
+	 * @param requester_id the requester_id to set
+	 */
+	public void setRequester_id(long requester_id) {
+		this.requester_id = requester_id;
+	}
+	/**
+	 * @return the assignee_id
+	 */
+	public long getAssignee_id() {
+		return assignee_id;
+	}
+	/**
+	 * @param assignee_id the assignee_id to set
+	 */
+	public void setAssignee_id(long assignee_id) {
+		this.assignee_id = assignee_id;
+	}
+	/**
+	 * @return the tags
+	 */
+	public ArrayList<String> getTags() {
+		return tags;
+	}
+	/**
+	 * @param tags the tags to set
+	 */
+	public void setTags(ArrayList<String> tags) {
+		this.tags = tags;
+	}
+	/**
+	 * @return the subject
+	 */
+	public String getSubject() {
+		return subject;
+	}
+	/**
+	 * @param subject the subject to set
+	 */
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	@Override
+	public String toString() {
+		return "Ticket [requester_id=" + requester_id + ", assignee_id=" + assignee_id + ", tags=" + tags + ", subject="
+				+ subject + ", description=" + description + "]";
+	}
+	
+}
