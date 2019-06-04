1852928561c0cfca8e343b8fdb68a2e30c8ed7fe
anita naseri <anita.naseri96@gmail.com>
2019-06-04 19:09:28 +1000

    method to display all tickets 25 per page, and ask user if it wants more tickets

--- src/main/TicketPresentation.java
+++ src/main/TicketPresentation.java
@@ -7,21 +7,69 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
-import model.Ticket;
-
 /**
  * @author anitanaseri
  *
  */
 public class TicketPresentation {
+	private final int TICKETS_IN_LIST = 25;
 	
+	// Display all the tickets from the HashMap, 25 tickets at a time.
+	public void showAllAvailableTickets(HashMap<Long, Ticket> ticketMap, Scanner scanner) {
+
+		ArrayList<Ticket> ticketsList = new ArrayList<>(ticketMap.values());
+	
+		
+		//if number of tickets less than 25 show all of them in one page
+		if (ticketsList.size() < TICKETS_IN_LIST) {
+			System.out.format("\n%-4s| %-4s | %-4s | %-50s | %-30s", "ID", "Requester ID", "Assignee ID", "SUBJECT","TAGS");
+			System.out.println(
+					"\n------------------------------------------------------------------------------------------------------------------------------------------------");
+
+			for (Ticket ticket : ticketsList) {
+				ShowIndividualTicket(ticket);
+			}
+		//if there are more than 25 tickets
+		} else {
+			int counter = 0;
+			int pageLimit = TICKETS_IN_LIST;
+			boolean headerFlag = true;
+			String input = "yes";
+			
+			while (counter < ticketsList.size() && input.toLowerCase().equals("yes")) {
+				if (headerFlag) {
+
+					System.out.format("\n%-4s| %-4s | %-4s | %-50s | %-30s", "ID", "Requester ID", "Assignee ID", "SUBJECT","TAGS");
+					System.out.println(
+							"\n------------------------------------------------------------------------------------------------------------------------------------------------");
+					headerFlag = false;
+				}
+				//display tickets from the hashmap one by one
+				ShowIndividualTicket(ticketsList.get(counter));
+				
+				//counter counts number of tickets which are from 0-100
+				counter++;
+
+				// when counter reaches 25 ask user if it wants more tickets if yes set header flag
+				//to true to print new header for new page
+				//pageLimit keeps track of what tickets per page, 0-25 first page, 25-50 second page,...
+				if (counter + 1> pageLimit) {
+					System.out.print("\n\n Do you wish to see more tickets? : ");
+					input = scanner.next();
+					pageLimit += TICKETS_IN_LIST;
+					headerFlag = true;
+				}
+			}
+		}
+	}
 
 	// Display ticket with the user input field ID as a key.
 	public void displayTicketById(HashMap<Long, Ticket> ticketMap, Long key) {
 		
 		//hashmap stores tickets by id as key, so we look for key input in keys of hashmap
 		if (ticketMap.containsKey(key)) {
-			System.out.format("\n%-4s| %-4s | %-4s | %-50s | %-50s | %-30s", "ID", "Requester ID", "Assignee ID", "SUBJECT", "description","TAGS");
+
+			System.out.format("\n%-4s| %-4s | %-4s | %-50s | %-30s", "ID", "Requester ID", "Assignee ID", "SUBJECT","TAGS");
 			System.out.println(
 					"\n------------------------------------------------------------------------------------------------------------------------------------------------");
 			ShowIndividualTicket(ticketMap.get(key));
@@ -41,6 +89,6 @@ public class TicketPresentation {
 		String description = ticket.getDescription();
 		
 		
-		System.out.format("%-4d| %-4d | %-4d | %-50s |  %-50s| %-50s\n", id, requester_id, assignee_id, subject, description, tags);
+		System.out.format("%-4d| %-4d | %-4d | %-50s| %-50s\n", id, requester_id, assignee_id, subject, tags);
 	}
 }
