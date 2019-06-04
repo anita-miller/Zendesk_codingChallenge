d677e17a01afddb21efe81a7a146395c2f178983
anita naseri <anita.naseri96@gmail.com>
2019-06-04 17:52:59 +1000

    get subdomain, email and password from user and pass to APICall

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,42 @@
+/**
+ * 
+ */
 package main;
 
+import java.util.Scanner;
+
+/**
+ * @author anitanaseri
+ *
+ */
 public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Authentication class to make REST API call with basic authorization.
+		APICall APIAuthorisation = new APICall();
+
+		Scanner scanner = new Scanner(System.in);
+		
+
+		System.out.println("\t Welcome to Zendesk Ticket Viewer Application");
+		System.out.print("\n Please Enter your Subdomain : ");
+				
+				
+		APIAuthorisation.setSubdomain(scanner.nextLine());
+
+		System.out.print("\n Please Enter your email address : ");
+				
+		APIAuthorisation.setUsername(scanner.nextLine());
+
+		System.out.print("\n Please Enter your password : ");
+		
+		APIAuthorisation.setPassword(scanner.nextLine());
+
+				
+
+	}
 
 }
